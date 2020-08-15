24a17d8ae8b6634bf5585441ab6200bc595e7dc5
Nick Hehr <nicholas.hehr@betterment.com>
2020-08-15 10:36:47 -0400

    feat: connect yarn workspaces, use core package in playground app

--- @playground/core/src/index.tsx
+++ @playground/core/src/index.tsx
@@ -1,11 +1,15 @@
 import React, { FC, HTMLAttributes, ReactChild } from 'react';
 
-export interface Props extends HTMLAttributes<HTMLDivElement> {
+export interface Props extends HTMLAttributes<HTMLParagraphElement> {
   children?: ReactChild;
 }
 
 // Please do not use types off of a default export module or else Storybook Docs will suffer.
 // see: https://github.com/storybookjs/storybook/issues/9556
-export const Thing: FC<Props> = ({ children }) => {
-  return <div>{children || `the snozzberries taste like snozzberries`}</div>;
+export const Thing: FC<Props> = ({ children, ...rest }) => {
+  return (
+    <p {...rest}>
+      <em>{children || `the snozzberries taste like snozzberries`}</em>
+    </p>
+  );
 };
--- playground/babel.config.js
+++ playground/babel.config.js
@@ -22,8 +22,10 @@ module.exports = function(api) {
         {
           targets: {
             node: 'current'
-          }
-        }
+          },
+          modules: 'commonjs'
+        },
+        '@babel/preset-react'
       ],
       (isProductionEnv || isDevelopmentEnv) && [
         '@babel/preset-env',
@@ -33,6 +35,13 @@ module.exports = function(api) {
           corejs: 3,
           modules: false,
           exclude: ['transform-typeof-symbol']
+        }
+      ],
+      [
+        '@babel/preset-react',
+        {
+          development: isDevelopmentEnv || isTestEnv,
+          useBuiltIns: true
         }
       ],
       ['@babel/preset-typescript', { 'allExtensions': true, 'isTSX': true }]
@@ -66,6 +75,12 @@ module.exports = function(api) {
         '@babel/plugin-transform-regenerator',
         {
           async: false
+        }
+      ],
+      isProductionEnv && [
+        'babel-plugin-transform-react-remove-prop-types',
+        {
+          removeImport: true
         }
       ]
     ].filter(Boolean)
